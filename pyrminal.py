a2fe5918a96de35779efc557e5ddc605dfc35ea1
Rocadinis <8b21espq@gmail.com>
2022-05-05 19:28:18 +0100

    Added more commands

--- pyrminal.py
+++ pyrminal.py
@@ -1,8 +1,9 @@
-# (C) Rodrigo Dinis
+# (C) Rodrigo Dinis, 2022
 import os
 user = os.getlogin()
 curDir = os.getcwd()
 os.system("cls")
+print("pyrminal by Rocadinis - May 2022 build\n")
 while True:
     command = input("$" + user + ": ")
     if command.startswith("echo "):
@@ -10,9 +11,12 @@ while True:
     elif command == "cd":
         print(curDir)
     elif command.startswith("cd "):
-        newDir = command[3:]
-        os.chdir(newDir)
-        curDir = os.getcwd()
+        try:
+            newDir = command[3:]
+            os.chdir(newDir)
+            curDir = os.getcwd()
+        except:
+            print("No such directory was found")
     elif command.startswith("mkdir "):
         try:
             os.mkdir(command[6:])
@@ -23,5 +27,35 @@ while True:
             os.rmdir(command[6:])
         except:
             print("No such directory was found")
+    elif command.startswith("type "):
+        try:
+            f = open(command[5:], "r")
+            content = f.readlines()
+            for line in content: print(line, end=" ")
+            print("") 
+        except:
+            print("A file with that name was not found")
+    elif command == "cls":
+        os.system("cls") #shameful
+    elif command.startswith("title "):
+        os.system("title " + command[6:]) #less shameful but still
+    elif command == "dir":
+        dirs = os.scandir(curDir)
+        for thing in dirs:
+            if thing.is_dir():
+                print("Folder -", thing.name)
+            if thing.is_file():
+                print("File -", thing.name)
+        print("\n")
+    elif command == "tree":
+        dirs = os.scandir(curDir)
+        for folder in dirs:
+            if folder.is_dir():
+                print("Main Folder name -", folder.name)
+                subDirs = os.scandir(folder)
+                for subthing in subDirs: 
+                    if subthing.is_dir(): print("Folder name -", subthing.name)
+                    if subthing.is_file(): print("File name -", subthing.name)
+                print("\n")
     else:
         print("command " + command + " was not found")
